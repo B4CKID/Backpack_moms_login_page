f61baec76300107308d8adc0f89ba6eb1c9ef8e6
Billy Barnes <86270229+B4CKID@users.noreply.github.com>
2022-01-20 17:27:04 +0000

    Added new js

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
-var b = $('p').on("mouseenter",function(){
-    $(this).css("font-size","25px");
-    $(this).css("background-color", "blue");
-})  
-var a = $('p').on("mouseleave",function(){
-    $(this).css("font-size"," ")
-    $(this).css("background-color", " ")
-})
-
-// function changeColor(){
-//   (b).css("background-color","purple")
-//   return false;
-// }
-
-var scheduleRide = $('button').on("click",function(){
-  window.location.href="https://www.facebook.com/backpackmoms2/posts/133859769042066"
-});
+// var scheduleRide = $('button').on("click",function(){
+//   window.location.href="https://www.facebook.com/backpackmoms2/posts/133859769042066"
+// });
 
 $(function () {
       $('#email').click(function (event) {
         var email = 'backpackmoms2@gmail.com';
-        var subject = 'Test';
-        var emailBody = 'Hi Sample,';
-        var attach = 'path';
+        var subject = '';
+        var emailBody = '';
+        var attach = '';
         document.location = "mailto:"+email+"?subject="+subject+"&body="+emailBody+
             "?attach="+attach;
       });
     });
+$(function () {
+      $('#b2').click(function (event) {
+        var email = 'backpackmoms2@gmail.com';
+        var subject = '';
+        var emailBody = '';
+        var attach = '';
+        document.location = "mailto:"+email+"?subject="+subject+"&body="+emailBody+
+            "?attach="+attach;
+      });
+    });
+$('p').mouseover(textChange);
+$('p').mouseout(textBack);
+
+
+function textChange(){
+    $(this).css("font-size","25px");
+}
+function textBack(){
+    $(this).css("background-color"," ");
+    $(this).css("font-size","18px");
+}
+         
