875803a1aa9112a968aef225a3d654e8bfe9b566
Billy Barnes <86270229+B4CKID@users.noreply.github.com>
2022-03-12 00:03:30 +0000

    V1

--- blog.js
+++ blog.js
@@ -2,18 +2,8 @@ var scheduleRide = $('button').on("click",function(){
   window.location.href="https://www.facebook.com/backpackmoms2/posts/133859769042066"
 });
 
-$(function () {
-      $('#email').click(function (event) {
-        var email = 'backpackmoms2@gmail.com';
-        var subject = '';
-        var emailBody = '';
-        var attach = '';
-        document.location = "mailto:"+email+"?subject:"+subject+"&body:"+emailBody+
-            "?attach="+attach;
-      });
-    });
 // $(function () {
-//       $('#b2').click(function (event) {
+//       $('#email').click(function (event) {
 //         var email = 'backpackmoms2@gmail.com';
 //         var subject = '';
 //         var emailBody = '';
@@ -22,6 +12,16 @@ $(function () {
 //             "?attach="+attach;
 //       });
 //     });
+$(function () {
+      $('#b2').click(function (event) {
+        var email = 'backpackmoms2@gmail.com';
+        var subject = '';
+        var emailBody = '';
+        var attach = '';
+        document.location = "mailto:"+email+"?subject:"+subject+"&body:"+emailBody+
+            "?attach="+attach;
+      });
+    });
 $('p').mouseover(textChange);
 $('p').mouseout(textBack);
 
